cb827a37d703a091935656d5d366996c36f5ecd6
Adam Akbar <adam.akbar@ruangguru.com>
2022-07-15 13:45:22 +0700

    feat(api): login

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  publicRuntimeConfig: {
+    JWT_SECRET: process.env.JWT_SECRET || "",
+    DATABASE_URL: process.env.DATABASE_URL || "",
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ pages/api/loginUser.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { compareHash } from "../../utils/auth";
+import { prisma } from "../../utils/prisma";
+import jwt from "jsonwebtoken";
+import getConfig from "next/config";
+
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
+
+
+const login = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+  if (!user) {
+    return res.status(401).send("Email not found");
+  } else {
+    const validPassword = await compareHash(password, user.password);
+    if (!validPassword) {
+      res.status(401).send("Invalid  password");
+    }
+    const token = jwt.sign({ user }, publicRuntimeConfig.JWT_SECRET);
+    res.setHeader("Set-Cookie", `token=${token}; httpOnly; path=/`);
+    return res.json({ token });
+
+  }
+
+}
+
+export default login;
