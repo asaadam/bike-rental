3854962bd2b5630218f218ec14d6930797b450e7
Adam Akbar <adam.akbar@ruangguru.com>
2022-07-16 14:11:09 +0700

    feat(api): add logic to get unbooked bike

--- pages/api/bike/getAllBike.ts
+++ pages/api/bike/getAllBike.ts
@@ -1,25 +1,88 @@
 import { prisma } from "../../../utils/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 import { checkValidToken } from "../../../utils/auth";
+import Joi from "joi";
 
 type Query = {
   page?: string,
-  limit?: string
+  limit?: string,
+  startDateQuery: string,
+  endDate: string,
 }
 
 const GetAllBike = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await checkValidToken(req);
     const query = req.query as unknown as Query;
-    const { page, limit } = query;
+    const { page, limit, startDateQuery, endDate } = query;
+    const validationQuery = Joi.object({
+      page: Joi.string().required(),
+      limit: Joi.string().required(),
+      startDateQuery: Joi.string().optional(),
+      endDate: Joi.string().optional(),
+    });
     let limitNumber;
     let ofset;
-    if (page && limit) {
-      const pageNumber = parseInt(page || '0');
-      limitNumber = parseInt(limit || '0');
-      ofset = (pageNumber - 1) * limitNumber;
+    if (validationQuery.validate(query).error) {
+      return res.status(400).json({ message: validationQuery.validate(query).error });
     }
+    const pageNumber = parseInt(page || '0');
+    limitNumber = parseInt(limit || '0');
+    ofset = (pageNumber - 1) * limitNumber;
+
     const bikes = await prisma.bike.findMany({
+      where: {
+        RentedBike: {
+          none: {
+            isCancled: false,
+            OR: [
+              {
+                AND: [
+                  {
+                    startDate: {
+                      lte: startDateQuery
+                    }
+                  },
+                  {
+                    endDate: {
+                      gte: startDateQuery
+                    }
+                  },
+                ],
+              },
+              {
+                AND: [
+                  {
+                    startDate: {
+                      lte: endDate
+                    }
+                  },
+                  {
+                    endDate: {
+                      gte: endDate
+                    }
+                  }
+                ]
+              }
+              ,
+              {
+                AND: [
+                  {
+                    startDate: {
+                      gte: startDateQuery
+                    }
+                  },
+                  {
+                    endDate: {
+                      lte: endDate
+                    }
+                  }
+                ]
+              },
+            ]
+          },
+        }
+      },
       skip: ofset ?? undefined,
       take: limitNumber ?? undefined,
       orderBy: {
